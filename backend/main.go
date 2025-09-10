de900acb94a17327813856404f7ef4acb28fbd20
Dalveer Singh <dalveersinghrehsi@gmail.com>
2025-09-10 17:26:36 +0530

    initail server setup

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/health", health)
+	router.Run("localhost:8080")
+}
+
+func health(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Server is running")
+}
