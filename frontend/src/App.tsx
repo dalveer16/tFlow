f779d0dea620d4842db317e43a057c928f68732a
Dalveer Singh <dalveersinghrehsi@gmail.com>
2025-09-16 12:34:14 +0530

    added dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <ModeToggle />
+      hi
     </div>
   );
 }
