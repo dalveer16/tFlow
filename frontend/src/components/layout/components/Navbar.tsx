8b7b26f78171df3954b2868049315ac71227c082
Dalveer Singh <dalveersinghrehsi@gmail.com>
2025-09-16 16:30:47 +0530

    navbar changes

--- frontend/src/components/layout/components/Navbar.tsx
+++ frontend/src/components/layout/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import React from "react";
 export default function Navbar() {
   return (
-    <header className="h-14 border-b flex items-center justify-between px-4">
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance font-mono">
-        tFlow{" "}
+    <header className="h-14 border-b flex items-center justify-between px-4 bg-sidebar">
+      <h1 className="scroll-m-20 text-center text-2xl md:text-2xl lg:text-4xl font-extrabold tracking-wider text-balance">
+        <span className="font-mono italic text-foreground/80">t</span>
+        <span className="font-sans not-italic text-foreground ">Flow</span>
       </h1>
 
       <div>
